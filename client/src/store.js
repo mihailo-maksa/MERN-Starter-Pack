1bd7d05e3bf49be079cf0b7e3f74e00fa01a5a88
Mihailo Maksa <57834287+mihailo-maksa@users.noreply.github.com>
2020-05-20 17:10:36 +0200

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
 
-const middlewares = [thunk];
+const middlewares = [];
 
 const store = createStore(
   rootReducer,
